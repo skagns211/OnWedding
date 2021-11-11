b849627636d3ee00dbddde3df4aa86edae02ac00
kkangtaeng <kimtaeng516@gmail.com>
2021-11-12 01:17:39 +0900

    Docs: Add App.js routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Article from "./Pages/Article";
+import ChangePassword from "./Pages/ChangePassword";
+import Delete from "./Pages/Delete";
+import Main from "./Pages/Main";
+import MyPage from "./Pages/MyPage";
+import SignUp from "./Pages/SignUp";
+import Write from "./Pages/Write";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Main />} />
+          {/* <Route path="/mypage" element={<MyPage />} />
+          <Route path="/delete" element={<Delete />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/write" element={<Write />} />
+          <Route path="change" element={<ChangePassword />} />
+          <Route path="article" element={<Article />} /> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
