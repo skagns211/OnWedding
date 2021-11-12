a39d6d611d3a674e4635f6b6728eddb2faa364d4
kkangtaeng <kimtaeng516@gmail.com>
2021-11-12 15:39:47 +0900

    Feat: Design main page
    라우터로 글쓰기 페이지, 회원가입 페이지, 메인 페이지 연결 구현
    간단한 메인 페이지 디자인

---
+++ client/src/Components/Nav.js
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const StyledNav = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #f4eae0;
+`;
+
+const StyledLink1 = styled.div`
+  padding-left: 2rem;
+  & a {
+    text-decoration: none;
+    color: black;
+  }
+`;
+
+const StyledLink2 = styled.ul`
+  display: flex;
+  list-style: none;
+  padding-left: 0;
+  & a {
+    padding: 1rem;
+    text-decoration: none;
+    color: black;
+  }
+`;
+
+const Nav = () => {
+  return (
+    <StyledNav>
+      <StyledLink1>
+        <Link to="/">Onwedding</Link>
+      </StyledLink1>
+      <StyledLink2>
+        <Link to="/signup">login</Link>
+        <Link to="/signup">signup</Link>
+      </StyledLink2>
+    </StyledNav>
+  );
+};
+
+export default Nav;
---
+++ client/src/Pages/SignUp.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Test = styled.div`
+  margin: 100px;
+`;
+
+const SignUp = () => {
+  return (
+    <div>
+      <Test>signup</Test>
+    </div>
+  );
+};
+
+export default SignUp;
