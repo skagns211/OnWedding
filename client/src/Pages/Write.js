f78310679f696a4a6154dac56c6a0ccf1317893e
kkangtaeng <kimtaeng516@gmail.com>
2021-11-13 12:46:44 +0900

    Feat: Design write page
    글쓰기 페이지 대략적인 폼 구현

--- client/src/Pages/Write.js
+++ client/src/Pages/Write.js
@@ -1,15 +1,79 @@
 import React from "react";
 import styled from "styled-components";
 
-const Test = styled.div`
-  margin: 100px;
+const StyledMain = styled.div`
+  display: flex;
+  background-color: #f4eae0;
+  align-items: center;
+`;
+
+const StyledBody = styled.div`
+  background-color: #f4eae0;
+  display: table-cell;
+  width: 100%;
+`;
+
+const StyledImg = styled.div`
+  background-image: url("https://i.ibb.co/x5HNV5z/bride-g8bfa369fe-1920.jpg");
+  background-position: center;
+  background-size: cover;
+  padding: 20rem;
+  opacity: 0.8;
+`;
+
+const StyledMiddle = styled.div`
+  background-color: #f4eae0;
+  padding: 2rem;
+`;
+
+const StyledContent = styled.div`
+  background-color: white;
+  padding: 2rem 5rem;
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledTitle = styled.input`
+  margin: 2rem;
+  padding: 0.5rem 20rem;
+`;
+
+const StyledText = styled.input`
+  margin: 2rem;
+  padding: 0rem 20rem 20rem 20rem;
+`;
+
+const StyledButton = styled.button`
+  border: 0px;
+  padding: 10px;
+  border-radius: 5px;
+  font-size: 1rem;
 `;
 
 const Write = () => {
   return (
-    <div>
-      <Test>Write</Test>
-    </div>
+    <StyledMain>
+      <StyledBody>
+        <StyledImg />
+        <StyledMiddle></StyledMiddle>
+        <StyledContent>
+          <div>글쓰기</div>
+          <div>
+            제목
+            <StyledTitle type="text" />
+          </div>
+          <div>
+            내용
+            <StyledText type="text" />
+          </div>
+          <div>
+            해시태그
+            <StyledTitle type="text" />
+          </div>
+          <StyledButton>글올리기</StyledButton>
+        </StyledContent>
+      </StyledBody>
+    </StyledMain>
   );
 };
 
