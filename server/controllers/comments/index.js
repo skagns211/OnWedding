cbe9aae0ae8cfbf43e6273eb76baa2c089b34399
steve <ionc635@naver.com>
2021-11-15 12:05:25 +0900

    Feat: construct PATCH comment API

--- server/controllers/comments/index.js
+++ server/controllers/comments/index.js
@@ -3,7 +3,25 @@ const { Comment } = require("../../models");
 module.exports = {
   comment: {
     post: (req, res) => {},
-    patch: (req, res) => {},
+    patch: async (req, res) => {
+      const id = req.params.id;
+      const message = req.body.message;
+
+      await Comment.update(
+        {
+          message,
+        },
+        {
+          where: { id },
+        }
+      );
+
+      try {
+        res.send('ok');
+      } catch (err) {
+        res.status(500).send();
+      }
+    },
     delete: (req, res) => {},
   },
 };
