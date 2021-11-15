64ba9fd378467873997bcf3384fb457a8dfe2f17
steve <ionc635@naver.com>
2021-11-16 03:14:02 +0900

    Feat: Construct API - article, comment

--- server/controllers/articles/index.js
+++ server/controllers/articles/index.js
@@ -1,4 +1,10 @@
-const { Article, Article_Hashtag, Hashtag } = require("../../models");
+const {
+  Article,
+  Article_Hashtag,
+  Hashtag,
+  Comment,
+  User,
+} = require("../../models");
 
 module.exports = {
   articles: {
@@ -8,45 +14,160 @@ module.exports = {
       if (!articles) {
         res.status(500).send();
       }
-      res.json({ data: articles });
+      res.json({ data: { articles } });
     },
   },
   article: {
-    post: (req, res) => {
+    post: async (req, res) => {
+      const { accessToken, tokenExpirse } = req.cookies;
+      if (tokenExpirse <= Date.now() / 1000) {
+        res
+          .clearCookie("accessToken")
+          .status(401)
+          .send({ message: "accessToken Expiration. plz Loing" });
+      } else if (!accessToken) {
+        res.status(403).send({ message: "not logged in" });
+      }
+
       const { title, message, image, hashtag } = req.body;
       const user_id = req.params.id;
-      if (!user_id || !title || !message || !image || !hashtag) {
+
+      if (!user_id || !title || !message) {
         res.status(400).send("bad request");
       }
 
-      Article.create({
+      const articleId = await Article.create({
         user_id,
         title,
         message,
         image,
-        hashtag,
-      })
-        .then(() => {
-          console.log("ok");
-        })
-        .catch(() => {
-          console.log("error");
+      });
+
+      for (const tag of hashtag) {
+        const [tags, created] = await Hashtag.findOrCreate({
+          where: { name: tag },
         });
-      res.send("createed");
+
+        await Article_Hashtag.create({
+          hashtag_id: tags.dataValues.id,
+          article_id: articleId.dataValues.id,
+        });
+
+        if (!created) {
+          await Article_Hashtag.create({
+            hashtag_id: tags.dataValues.id,
+            article_id: articleId.dataValues.id,
+          });
+        }
+      }
+
+      try {
+        res.send("created");
+      } catch (err) {
+        res.status(500).send();
+      }
     },
     get: async (req, res) => {
       const id = req.params.id;
       const article = await Article.findOne({
+        include: [
+          {
+            model: User,
+            attributes: ["name"],
+          },
+        ],
         where: { id },
+      });
+      const comments = await Comment.findAll({
+        where: { article_id: id },
       });
 
       if (!article) {
         res.status(500).send();
       }
-      res.json({ data: article });
+      res.json({ data: { article, comments } });
     },
-    patch: (req, res) => {},
-    delete: (req, res) => {},
+    patch: async (req, res) => {
+      const { accessToken, tokenExpirse } = req.cookies;
+      if (tokenExpirse <= Date.now() / 1000) {
+        res
+          .clearCookie("accessToken")
+          .status(401)
+          .send({ message: "accessToken Expiration. plz Loing" });
+      } else if (!accessToken) {
+        res.status(403).send({ message: "not logged in" });
+      }
+
+      const id = req.params.id;
+      const { title, message, image, hashtag } = req.body;
+
+      if (!title || !message) {
+        res.status(400).send("bad request");
+      }
+      await Article.update(
+        {
+          title,
+          message,
+          image,
+        },
+        {
+          where: { id },
+        }
+      );
+
+      await Article_Hashtag.destroy({
+        where: { article_id: id },
+      });
+
+      for (const tag of hashtag) {
+        const [tags, created] = await Hashtag.findOrCreate({
+          where: { name: tag },
+        });
+        await Article_Hashtag.create({
+          hashtag_id: tags.dataValues.id,
+          article_id: id,
+        });
+        if (!created) {
+          await Article_Hashtag.create({
+            hashtag_id: tags.dataValues.id,
+            article_id: id,
+          });
+        }
+      }
+
+      try {
+        res.status(201).send("updated");
+      } catch (err) {
+        res.status(500);
+      }
+    },
+    delete: async (req, res) => {
+      const { accessToken, tokenExpirse } = req.cookies;
+      if (tokenExpirse <= Date.now() / 1000) {
+        res
+          .clearCookie("accessToken")
+          .status(401)
+          .send({ message: "accessToken Expiration. plz Loing" });
+      } else if (!accessToken) {
+        res.status(403).send({ message: "not logged in" });
+      }
+
+      const id = req.params.id;
+
+      await Article_Hashtag.destroy({
+        where: { article_id: id },
+      });
+
+      await Article.destroy({
+        where: { id },
+      });
+
+      try {
+        res.status(204).send("ok");
+      } catch (err) {
+        res.status(500).send();
+      }
+    },
   },
   hashtag: {
     get: async (req, res) => {
@@ -68,7 +189,7 @@ module.exports = {
       if (!articles) {
         res.status(500).send();
       }
-      res.json({ data: articles });
+      res.json({ data: { articles } });
     },
   },
 };
--- server/controllers/users/index.js
+++ server/controllers/users/index.js
@@ -1,85 +1,92 @@
 const { User } = require("../../models");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const { access } = require("fs");
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   password: {
     post: async (req, res) => {
       const { accessToken } = req.cookies;
       const { tokenExpirse } = req.cookies;
-      if (tokenExpirse <= (Date.now() / 1000)) {
-        res.clearCookie('accessToken').status(401).send({ message: 'accessToken Expiration. plz Loing' })
+      if (tokenExpirse <= Date.now() / 1000) {
+        res
+          .clearCookie("accessToken")
+          .status(401)
+          .send({ message: "accessToken Expiration. plz Loing" });
       } else if (!accessToken) {
-        res.status(403).send({ message: 'not logged in' })
+        res.status(403).send({ message: "not logged in" });
       }
       const { password } = req.body;
       const loginUser = jwt.verify(accessToken, process.env.ACCESS_SECRET);
       const { id } = loginUser;
       const userInfo = await User.findOne({
-        where: { id }
-      })
-      console.log(userInfo)
+        where: { id },
+      });
+      console.log(userInfo);
       if (userInfo.password === password) {
         try {
-          res.send({ message: 'valid password' })
+          res.send({ message: "valid password" });
         } catch (err) {
-          console.log(err)
+          console.log(err);
         }
       } else {
-        res.send({ message: 'invalid password' })
+        res.send({ message: "invalid password" });
       }
-
     },
-    patch: (req, res) => {
-
-    },
+    patch: (req, res) => {},
   },
   userInfo: {
     get: async (req, res) => {
       const { accessToken } = req.cookies;
       const { tokenExpirse } = req.cookies;
       // accessToken 만료 여부 체크
-      if (tokenExpirse <= (Date.now() / 1000)) {
-        res.clearCookie('accessToken').status(401).send({ message: 'accessToken Expiration. plz Loing' })
+      if (tokenExpirse <= Date.now() / 1000) {
+        res
+          .clearCookie("accessToken")
+          .status(401)
+          .send({ message: "accessToken Expiration. plz Loing" });
       } else if (accessToken) {
-        const userInfo = await jwt.verify(accessToken, process.env.ACCESS_SECRET);
+        const userInfo = await jwt.verify(
+          accessToken,
+          process.env.ACCESS_SECRET
+        );
         try {
-          res.send({ data: { userInfo }, message: 'present Userinfo' })
+          res.send({ data: { userInfo }, message: "present Userinfo" });
         } catch (err) {
-          console.log(err)
+          console.log(err);
         }
       } else {
-        res.status(403).send({ message: 'not logged in' })
+        res.status(403).send({ message: "not logged in" });
       }
     },
     delete: async (req, res) => {
       const { accessToken } = req.cookies;
       const { tokenExpirse } = req.cookies;
-      if (tokenExpirse <= (Date.now() / 1000)) {
-        res.clearCookie('accessToken').status(401).send({ message: 'accessToken Expiration. plz Loing' })
+      if (tokenExpirse <= Date.now() / 1000) {
+        res
+          .clearCookie("accessToken")
+          .status(401)
+          .send({ message: "accessToken Expiration. plz Loing" });
       } else if (accessToken) {
         const userInfo = jwt.verify(accessToken, process.env.ACCESS_SECRET);
         const { id, email } = userInfo;
         const loginInfo = await User.findOne({
-          where: { id, email }
-        })
-        console.log(loginInfo)
+          where: { id, email },
+        });
+        console.log(loginInfo);
         await User.destroy({
-          where: { id, email }
-        })
+          where: { id, email },
+        });
         try {
-          res.clearCookie('accessToken')
-          res.send({ message: 'success delete userInfo' })
+          res.clearCookie("accessToken");
+          res.send({ message: "success delete userInfo" });
         } catch (err) {
-          console.log(err)
+          console.log(err);
         }
       }
     },
   },
   profile: {
-    patch: (req, res) => {
-
-    },
+    patch: (req, res) => {},
   },
 };
