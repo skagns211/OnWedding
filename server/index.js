0e0283c5b0f1bef17a1b01c12fee46fc76a94a3e
김남훈 <skagns211@gmail.com>
2021-11-17 22:08:50 +0900

    Fix: server/index.js Fix

--- server/index.js
+++ server/index.js
@@ -1,36 +1,21 @@
-require("dotenv").config();
-const fs = require("fs");
-const https = require("https");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const indexRouter = require("./routes");
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: ["https://localhost:3000"],
+    origin: ["http://localhost:80"],
     credentials: true,
     methods: ["GET", "POST", "OPTIONS", "PATCH", "DELETE"],
   })
-);
+)
 app.use(cookieParser());
 
 app.use("/", indexRouter);
 
-const HTTPS_PORT = process.env.HTTPS_PORT || 4000;
+const port = 80;
 
-let server;
-if (fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")) {
-  const privateKey = fs.readFileSync(__dirname + "/key.pem", "utf8");
-  const certificate = fs.readFileSync(__dirname + "/cert.pem", "utf8");
-  const credentials = { key: privateKey, cert: certificate };
-
-  server = https.createServer(credentials, app);
-  server.listen(HTTPS_PORT, () => console.log("server runnning"));
-} else {
-  server = app.listen(HTTPS_PORT);
-}
-module.exports = server;
+app.listen(port, () => {
+  console.log("server on! http://localhost:" + port);
+});
